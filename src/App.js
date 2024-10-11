524bd6743482af18272a049fdb142cae20789013
renatofernandosilvagoncalves <renatogoncalves@unoeste.edu.br>
2024-10-11 08:49:48 -0300

    preparando o deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
+      {
+        //antes
+        //http://localhost:3000/
+
+        //depois
+        //http://localhost:3000/projetoeleicoes2024
+      }
+        <BrowserRouter basename="/projetoeleicoes2024">
             <Routes>
                 <Route path="/" element={<TelaPrincipal />} />
             </Routes>
